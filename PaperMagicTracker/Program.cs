4459085a64e0c7436aa51567071c04c7829c6f8e
Joschi Grey <joschi.grey@posteo.de>
2022-01-23 22:36:24 +0100

    Changed Syncfusion to local nuget

--- PaperMagicTracker/Program.cs
+++ PaperMagicTracker/Program.cs
@@ -6,7 +6,6 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using PaperMagicTracker;
 using PaperMagicTracker.Classes;
 using Syncfusion.Blazor;
-using Syncfusion.Licensing;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -14,15 +13,12 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddSyncfusionBlazor();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-builder.Services.AddLogging(logger => logger.AddBrowserConsole().SetMinimumLevel(LogLevel.Trace));
+builder.Services.AddLogging(logger => logger.AddBrowserConsole().SetMinimumLevel(LogLevel.Information));
 
 builder.Services.AddStorage();
 
 builder.Services.AddSingleton<AppState>();
 
 var client = new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)};
-
-var licenseKey = client.GetStringAsync(@"sample-data\/SyncfusionLicense.txt");
-SyncfusionLicenseProvider.RegisterLicense(await licenseKey);
 
 await builder.Build().RunAsync();
